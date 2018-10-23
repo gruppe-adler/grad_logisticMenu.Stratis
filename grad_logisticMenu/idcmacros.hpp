143f36d34b4973d2000615a2cc05a51ad15aadc0
DerZade <DerZade@gmail.com>
2018-10-23 22:45:30 +0200

    Minor tweaks

--- grad_logisticMenu/idcmacros.hpp
+++ grad_logisticMenu/idcmacros.hpp
@@ -1,8 +1,6 @@
 //macros
 #define GRID_W (pixelW * pixelGrid * 0.5)
 #define GRID_H (pixelH * pixelGrid * 0.5)
-#define CONTROLS_W 120
-#define CONTROLS_H 80
 #define ACTIVECOLOR {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.77])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.51])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.08])", 1}
 
 //IDCs
